136a04d19cb1ce4b1cbb7703aee63b03ad362c1d
Walker Crouse <walkercrouse@hotmail.com>
2016-11-01 13:39:29 -0400

    Import custom fields with script
    Signed-off-by: Walker Crouse <walkercrouse@hotmail.com>

--- scripts/importDiscourseUsers.py
+++ scripts/importDiscourseUsers.py
@@ -34,31 +34,86 @@ except:
 print("Connection established.")
 
 curIn = connIn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-curIn.execute("SELECT * from users WHERE username != 'system';")
-users = curIn.fetchall()
+curOut = connOut.cursor()
+
+importUsers = len(sys.argv) == 1
+if importUsers:
+    # Import users
+    query = "SELECT * from users WHERE username != 'system';"
+    print(query)
+    curIn.execute(query)
+    users = curIn.fetchall()
+
+    importStmt = "INSERT INTO users (created_at, email, username, password, salt, is_admin) VALUES \n"
+    values = "('%s', '%s', '%s', '%s', '%s', %s)"
+    usersLen = len(users)
+
+    print("Importing %d users..." % usersLen)
+    for i, user in enumerate(users):
+        username = user['username']
+        email = user['email']
+        password = user['password_hash']
+        salt = user['salt']
+        admin = user['admin']
+        importStmt += values % (datetime.now(), email, username, password, salt, admin)
+        if (i < usersLen - 1):
+            importStmt += ',\n'
+
+    importStmt += ';'
+    print(importStmt)
+    curOut.execute(importStmt)
+
+    confirm = input("Commit changes? (y/n): ")
+    if confirm.lower() == 'y':
+        connOut.commit()
+
+# Import user fields?
+importUserFields = len(sys.argv) > 1 and sys.argv[1] == "--customFields"
+if importUserFields:
+    # Map the custom field names to our corresponding output name
+    query = "SELECT DISTINCT name FROM user_custom_fields;"
+    print(query)
+    curIn.execute(query)
+    fieldNames = curIn.fetchall()
+    fieldNameMap = {}
+    for fieldName in fieldNames:
+        outName = input("What is " + fieldName[0] + " called in the output database?: ")
+        fieldNameMap[fieldName[0]] = outName
+
+    # Get the usernames for the user IDs we are importing
+    query = "SELECT DISTINCT user_id FROM user_custom_fields;"
+    print(query)
+    curIn.execute(query)
+    userIds = curIn.fetchall()
+    userMap = {}
+    for userId in userIds:
+        query = "SELECT username FROM users WHERE id = %d;" % userId[0]
+        print(query)
+        curIn.execute(query)
+        username = curIn.fetchone()
+        userMap[userId[0]] = username[0]
+
+    # Import the fields
+    query = "SELECT * FROM user_custom_fields;"
+    print(query)
+    curIn.execute(query)
+    fields = curIn.fetchall()
+    for field in fields:
+        inName = field['name']
+        outName = fieldNameMap[inName]
+        value = field['value']
+        username = userMap[field['user_id']]
+        importStmt = "UPDATE users SET %s = '%s' WHERE username = '%s';" % (outName, value, username)
+        print(importStmt)
+        curOut.execute(importStmt)
+
+    confirm = input("Commit changes? (y/n): ")
+    if confirm.lower() == 'y':
+        connOut.commit()
+
+# Clean up
 curIn.close()
 connIn.close()
-
-importStmt = "INSERT INTO users (created_at, email, username, password, salt, is_admin) VALUES \n"
-values = "('%s', '%s', '%s', '%s', '%s', %s)"
-usersLen = len(users)
-
-print("Importing %d users..." % usersLen)
-for i, user in enumerate(users):
-    username = user['username']
-    email = user['email']
-    password = user['password_hash']
-    salt = user['salt']
-    admin = user['admin']
-    importStmt += values % (datetime.now(), email, username, password, salt, admin)
-    if (i < usersLen - 1):
-        importStmt += ',\n'
-
-importStmt += ';'
-print(importStmt)
-curOut = connOut.cursor()
-curOut.execute(importStmt)
-connOut.commit()
 
 curOut.close()
 connOut.close()
